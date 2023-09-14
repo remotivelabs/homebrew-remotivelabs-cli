43fb10f8fd6bcd3f8f401a30bea98edf880fae56
Johan Rask <johan.rask@remotivelabs.com>
2023-09-14 10:56:34 +0200

    Added macos-13 + new version
    Closes #5.
    
    Signed-off-by: Johan Rask <johan.rask@klipsk.se>

--- Formula/remotivelabs-cli.rb
+++ Formula/remotivelabs-cli.rb
@@ -2,8 +2,8 @@ class RemotivelabsCli < Formula
   include Language::Python::Virtualenv
   desc "CLI for managing RemotiveLabs cloud and brokers"
   homepage "https://remotivelabs.com"
-  url "https://files.pythonhosted.org/packages/03/9c/f4693af83d0c58cbfb3363561ef7b0f2214197d5cf16867276a337ddd8d1/remotivelabs_cli-0.0.1a25.tar.gz"
-  sha256 "d0350dcc3cc176b6974f86019c6d76fd551ae2e348cd5a87ea688743d53ba8c8"
+  url "https://files.pythonhosted.org/packages/67/16/661b00b9e96e680e2135bb04d23db93fef270602ce47fc81420ba85e6672/remotivelabs_cli-0.0.1a26.tar.gz"
+  sha256 "8377418add3c0aee77761da9b47aa1f5b479eb34b9af89da1a6bec7f35c6b752"
   license "Apache-2.0"
 
   livecheck do
@@ -120,6 +120,6 @@ class RemotivelabsCli < Formula
   end
 
   test do
-    assert_equal "remotivelabs-cli 0.0.1a25", shell_output("#{bin}/remotive --version").strip
+    assert_equal "remotivelabs-cli 0.0.1a26", shell_output("#{bin}/remotive --version").strip
   end
 end
